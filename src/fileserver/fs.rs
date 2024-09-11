6d166d03f2fa13eca6c8bada71d9d9c6c557e925
kauncoder <snehsocial@protonmail.com>
2024-09-11 09:24:30 +0200

    sanitising file names

--- src/fileserver/fs.rs
+++ src/fileserver/fs.rs
@@ -8,10 +8,15 @@ use std::sync::Arc;
 use futures::TryStreamExt;
 use warp::{self, Buf,http::StatusCode};
 use crate::merkletree::tree::FastMerkleTree;
+use anyhow::Result;
+use regex::Regex;
 
 const UPLOAD_DIR: &str = "filestore/";
 
 pub async fn handle_file_upload(db: Arc<sled::Db>,mut form: FormData) -> Result<impl warp::Reply, Infallible> {
+
+    //empty the current folder for new uploads since user can't have root hash for all files
+    let _ = empty_folder(format!("./{}",UPLOAD_DIR));
 
     while let Ok(Some(part)) = form.try_next().await {
         let filename = match part.filename() {
@@ -24,18 +29,20 @@ pub async fn handle_file_upload(db: Arc<sled::Db>,mut form: FormData) -> Result<
         while let Ok(Some(chunk)) = stream.try_next().await {
             data.extend_from_slice(chunk.chunk());
         }
-
-        let save_path = PathBuf::from(format!("./{}{}",UPLOAD_DIR, filename));
+        //clean file name for storage (remove all spaces and special characters)
+        let clean_file_name = clean_file_name(&filename);
+        let save_path = PathBuf::from(format!("./{}{}",UPLOAD_DIR, clean_file_name));
         if (tokio::fs::write(save_path, data).await).is_err() {
             return Ok(StatusCode::INTERNAL_SERVER_ERROR);
         }
     }
 
+    //clear old db entries before adding new ones
+    let _ = clear_db(db.clone());
     //build merkle tree for the entire upload folder
     let file_hash_map = get_file_list();
     FastMerkleTree::build_merkle_tree (db,file_hash_map);
-    Ok(StatusCode::CREATED)
-
+    Ok(StatusCode::OK)
 
 }
 
@@ -90,4 +97,35 @@ fn get_file_list()->Vec<String>{
         file_list.push(path.display().to_string())
      }
      file_list
+}
+
+fn clear_db(db: Arc<sled::Db>)-> Result<()>{
+    for result in db.iter() {
+        let (key, _) = result?;
+        db.remove(key)?;
+    }
+ //   db.flush()?;
+    Ok(())
+}
+
+fn empty_folder(path: String) -> Result<()> {
+
+    let folder_path = Path::new(&path);
+    for entry in fs::read_dir(folder_path)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            fs::remove_dir_all(&path)?;
+        } else {
+            fs::remove_file(&path)?
+        }
+    }
+    Ok(())
+}
+
+fn clean_file_name(file_name: &str)->String{
+        let invalid_chars = Regex::new(r"[^\w\.\-]").unwrap();
+        let trimmed = file_name.trim();
+        let clean_file_name = invalid_chars.replace_all(trimmed, "_");
+        clean_file_name.to_string()
 }
